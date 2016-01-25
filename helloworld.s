2cf251c407c33757c9323ceafa68875b7f04d33b
michaelluang <michaelluang@gmail.com>
2016-01-25 13:35:21 +0800

    Hello, World!

---
+++ helloworld.s
@@ -0,0 +1,29 @@
+.code32
+
+# PURPOSE: This program prints out message "Hello, World!" to
+#          STDOUT and exits.
+
+.include "linux.s"
+
+.section .data
+
+ hello_world:
+  .ascii "Hello, World!\n"
+ hello_world_end:
+
+ .equ hello_world_len, (hello_world_end - hello_world) 
+
+.section .text
+
+.globl _start
+
+_start:
+ movl $STDOUT, %ebx
+ movl $hello_world, %ecx
+ movl $hello_world_len, %edx
+ movl $SYS_WRITE, %eax
+ int $LINUX_SYSCALL
+
+ movl $0, %ebx
+ movl $SYS_EXIT, %eax
+ int $LINUX_SYSCALL
